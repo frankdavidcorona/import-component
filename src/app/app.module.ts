f996a0876d5cb422f2e89273ce0953b757f30868
frankdavidcorona <frankdavid.corona@gmail.com>
2017-06-19 11:31:19 -0400

    Add component an dependencies of Covalent teradata.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,15 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+// Angular animations
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+// Covalent Teradata
+import { CovalentLayoutModule} from '@covalent/core';
+// (optional) Additional Covalent Modules imports
+import { CovalentHttpModule } from '@covalent/http';
+import { CovalentHighlightModule } from '@covalent/highlight';
+import { CovalentMarkdownModule } from '@covalent/markdown';
+import { CovalentDynamicFormsModule } from '@covalent/dynamic-forms';
 
 import {AppComponent} from './app.component';
 
@@ -10,7 +19,12 @@ import {AppComponent} from './app.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CovalentDynamicFormsModule,
+    CovalentHighlightModule,
+    CovalentMarkdownModule,
+    CovalentHttpModule,
+    CovalentLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
