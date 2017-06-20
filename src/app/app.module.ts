3f49d087b9bbf5573c79b62f3fcc4b3b3b0a4f82
frankdavidcorona <frankdavid.corona@gmail.com>
2017-06-20 09:15:36 -0400

    Change default route path.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,7 +24,7 @@ import {CovalentDynamicFormsModule} from '@covalent/dynamic-forms';
 const ROUTES = [
   {
     path: '',
-    redirectTo: 'posts',
+    redirectTo: '/',
     pathMatch: 'full'
   },
   {
